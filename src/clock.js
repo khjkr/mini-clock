ff2f8d4f33a72fd7a9d8124f06123ef845502046
Hyunjun Kim <hyunjun5216@naver.com>
2021-01-30 15:50:48 +0900

    realtime clock

--- src/clock.js
+++ src/clock.js
@@ -1,16 +1,19 @@
 const clockContainer = document.querySelector(".clock"),
-   clockTitle = clockContainer.querySelector("h1")
+  clockTitle = clockContainer.querySelector("h1")
 
- function getTime() {
-   const date = new Date()
-   const hours = date.getHours()
-   const minutes = date.getMinutes()
-   const seconds = date.getSeconds()
-   clockTitle.innerText = `${hours}:${minutes}:${seconds}`
- }
+function getTime() {
+  const date = new Date()
+  const hours = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+  
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`
+}
 
- function init() {
-   getTime()
- }
+function init() {
+  setInterval(getTime, 1000);
+}
 
- init()
+init()
