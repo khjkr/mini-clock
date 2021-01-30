4e1daeae2beffcf5e663338fb1920ad1a43a4815
Hyunjun Kim <hyunjun5216@naver.com>
2021-01-30 15:20:13 +0900

    get date

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 400,
+    height: 200
   })
 
   // and load the index.html of the app.
